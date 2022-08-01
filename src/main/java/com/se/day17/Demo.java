f83bc205dbe23de0eccfac19a1f779cfaaf126a6
guoyougen <2942000865@qq.com>
2022-08-01 13:48:39 +0800

    Initial commit

---
+++ src/main/java/com/se/day17/Demo.java
@@ -0,0 +1,13 @@
+package com.se.day17;
+/**
+ * @projectname: javalearncode
+ * @packname: com.se.day17
+ * @classname: Demo
+ * @author: gyg
+ * @date: 2022/8/1 下午 1:46
+ * @version: 1.0
+ * @description:
+*/
+
+public class Demo {
+}
